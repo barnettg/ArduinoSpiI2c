b48729a339481ab6a4c9141e36475e49ff87a99b
GlenBarnett <glen.barnett@coherent.com>
2018-10-22 18:07:07 -0700

    add to MCP23017 class

--- sketch_spi/MCP23017.cpp
+++ sketch_spi/MCP23017.cpp
@@ -1,4 +1,5 @@
 // cpp file for MCP23017
+#include <Arduino.h>
 #include "MCP23017.h"
 
 
@@ -10,35 +11,110 @@ MCP23017::MCP23017(unsigned char I2C_address,unsigned char portAdir, unsigned ch
 
 void MCP23017::writeReg(unsigned char reg, unsigned char data)
 {
+  Wire.beginTransmission(_I2C_address);
+  Wire.write(reg);
+  Wire.write(data);   
+  Wire.endTransmission();
 }
 unsigned char MCP23017::readReg(unsigned char reg)
 {
-	return 0;
+  unsigned char result=0;
+  Wire.beginTransmission(_I2C_address);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(_I2C_address,1);
+  if(Wire.available()<=1) result = Wire.read(); //???????????????????????
+	return result;
 }  
 
 void MCP23017::initialize(unsigned char portAdir, unsigned char portBdir)
 {
+   // set up port A 
+   writeReg(IODIRA, portAdir);
+   
+    // set up port B  
+   writeReg(IODIRB, portBdir);
+   delay(100); // wait 100 ms
+      
+   // set port B
+   writeReg(GPIOB, 0xFF);
+   // set port A
+   writeReg(GPIOA, 0xFF);
+   delay(100); // wait 100 ms
 }
 
 unsigned char MCP23017::readPortA(void)
 {
-	return 0;
+  return readReg(GPIOA);
 }
 unsigned char MCP23017::readPortB(void)
 {
-	return 0;
+	return readReg(GPIOB);
 }
 
 void MCP23017::writePortA(unsigned char value)
 {
-
+  writeReg(GPIOA, value);
 }
 void MCP23017::writePortB(unsigned char value)
 {
-
+  writeReg(GPIOB, value);
 }
-	
-	
 
+/// read bit from portA
+///
+/// @param bit number 0-7
+/// @return bit value
+bool MCP23017::readPortaBit(int bitNumber)
+{
+   int val = readPortA();
+   int mask = 0x01 << (bitNumber & (0x7));
+   if ((val & mask) != 0) return true;
+   return false;
+}
+
+/// read bit from portB
+///
+/// @param bit number 0-7
+/// @return bit value
+bool MCP23017::readPortbBit(int bitNumber)
+{
+   int val = readPortB();
+   int mask = 0x01 << (bitNumber & (0x7));
+   if ((val & mask) != 0) return true;
+   return false;
+}
+
+/// write bit on portA
+///
+/// @param bit number 0-7 and value true/false
+/// @return true if no error
+void MCP23017::writePortaBit(int bitNumber, bool logicVal)
+{
+   int val = readPortA();
+   int mask = 0x01 << (bitNumber & (0x7));
+   if(logicVal)
+      val |= mask;
+   else
+      val &= ~mask;
+
+   writePortA(val);
+}
+
+/// write bit on portB
+///
+/// @param bit number 0-7 and value true/false
+/// @return true if no error
+void MCP23017::writePortbBit(int bitNumber, bool logicVal)
+{ 
+   int val = readPortB();
+   int mask = 0x01 << (bitNumber & (0x7));
+   if(logicVal)
+      val |= mask;
+   else
+      val &= ~mask;
+
+   writePortB(val);
+}
 
 
--- sketch_spi/MCP23017.h
+++ sketch_spi/MCP23017.h
@@ -1,6 +1,8 @@
 // header file for AMC7812
 #ifndef __MCP23017__H_
 #define __MCP23017__H_
+#include <Wire.h>
+
 class MCP23017
 {
   public:
@@ -16,6 +18,46 @@ class MCP23017
 	
 	void writePortA(unsigned char value);
 	void writePortB(unsigned char value);
+
+  bool readPortaBit(int bitNumber);
+  bool readPortbBit(int bitNumber);
+  void writePortaBit(int bitNumber, bool logicVal);
+  void writePortbBit(int bitNumber, bool logicVal);
+  
+
+    typedef enum  
+   {
+      IODIRA = 0,
+      IODIRB,
+      IPOLA,
+      IPOLB,
+      GPINTENA,
+      GPINTENB,
+      DEFVALA,
+      DEFVALB,
+      INTCONA,
+      INTCONB,
+      IOCONA,
+      IOCONB,
+      GPPUA ,
+      GPPUB,
+      INTFA,
+      INTFB,
+      INTCAPA,
+      INTCAPB,
+      GPIOA = 0x12,
+      GPIOB = 0x13,
+      OLATA,
+      OLATB
+   }Registers;
+
+   typedef enum  
+   {
+      DOUT  = 0x00,
+      DIN = 0xFF
+   }Direction;
+
+   
     
   private:
    unsigned char _I2C_address;
